5cb41dcc269c6188da5c1646f834a3eff659cac3
Fabio Di Cillo <fabiodicillo@gmail.com>
2024-02-28 12:37:12 +0100

    USER STORY TERMINATE...THE END

--- resources/views/team/teampage.blade.php
+++ resources/views/team/teampage.blade.php
@@ -6,7 +6,7 @@
                 <h3 class="">Codifica Corggiosa è un team di sviluppatori web che si occupa di creare siti e applicazioni innovative, funzionali e creative. Il loro motto è: "Non siamo solo programmatori, siamo artisti del codice". Il team è composto da persone appassionate, competenti e sempre aggiornate sulle ultime tecnologie e tendenze del web. Codifica Corggiosa offre soluzioni personalizzate e su misura per ogni tipo di cliente, dal piccolo imprenditore alla grande azienda, garantendo qualità, sicurezza e soddisfazione. Codifica Corggiosa è il team che fa per te se cerchi un partner affidabile, professionale e corggioso per i tuoi progetti web.</h3>
             </div>
             <div class="col-4">
-                <img class="logoTeam" src="{{ asset('storage/img/logominimal2.png') }}" alt="logoCodificaCoraggiosa"> 
+                <img class="logoTeam" src="{{ asset('storage/img/logominimal.png') }}" alt="logoCodificaCoraggiosa"> 
             </div>
         </div>
     </div>
@@ -28,7 +28,7 @@
                         <h4 class="font-title">Fabio Di Cillo</h4>
                         <p style="font-style: italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quod laudantium similique debitis!</p>
                         <div class="OUR">
-                        <a href="https://www.linkedin.com/"> <ion-icon name="logo-linkedin"></ion-icon></a></href>
+                        <a href="https://www.linkedin.com/" > <ion-icon name="logo-linkedin"></ion-icon></a></href>
                         </div>
                     </div>
                     <div class="col-2 mx-1">
@@ -36,7 +36,7 @@
                         <h4 class="font-title">Francesco Loconte</h4>
                         <p style="font-style: italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quod laudantium similique debitis!</p>
                         <div class="OUR">
-                        <a href="https://www.linkedin.com/"> <ion-icon name="logo-linkedin"></ion-icon></a></href>
+                        <a href="https://www.linkedin.com/" > <ion-icon name="logo-linkedin"></ion-icon></a></href>
                         </div>
                     </div>
                     <div class="col-2 mx-1">
@@ -44,7 +44,7 @@
                         <h4 class="font-title">Andrea Morosetti</h4>
                         <p style="font-style: italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quod laudantium similique debitis!</p>
                         <div class="OUR">
-                        <a href="https://www.linkedin.com/"> <ion-icon name="logo-linkedin"></ion-icon></a></href>
+                        <a href="https://www.linkedin.com/" > <ion-icon name="logo-linkedin"></ion-icon></a></href>
                         </div>
                     </div>
                     <div class="col-2 mx-1">
@@ -52,7 +52,7 @@
                         <h4 class="font-title">Stefano Princiotto</h4>
                         <p style="font-style: italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quod laudantium similique debitis!</p>
                         <div class="OUR">
-                        <a href="https://www.linkedin.com/"> <ion-icon name="logo-linkedin"></ion-icon></a></href>
+                        <a href="https://www.linkedin.com/" > <ion-icon name="logo-linkedin"></ion-icon></a></href>
                         </div>
                     </div>
                     </div>
